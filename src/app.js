b68fa70b8d05b6b52ba16cd27b9cef495d901d25
Samuel Araújo Silva <61428843+SamuelSilvaAraujo@users.noreply.github.com>
2024-03-04 15:48:02 -0300

    iniciando controllers e routers

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
 
+// Routers
+
+const bookRoute = require("./routers/book");
+
+// Api
+
+app.use("/api/books", bookRoute);
+
+// Frontend
+
 module.exports = app;
---
+++ src/controllers/book.js
@@ -0,0 +1,4 @@
+exports.getAll = (req, res, next) => {
+  console.log("samuel");
+  res.status(200).send("Get All");
+};
