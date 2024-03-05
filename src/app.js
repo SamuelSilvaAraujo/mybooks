abfb4421cdd9638c82c03a4737405103234549b1
Samuel Araújo Silva <61428843+SamuelSilvaAraujo@users.noreply.github.com>
2024-03-05 15:24:54 -0300

    add crud de livro

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ const app = express();
 // Database
 require("./database/db");
 
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Routers
 const bookRoute = require("./routers/book");
 
--- src/controllers/book.js
+++ src/controllers/book.js
@@ -1,17 +1,62 @@
-exports.getAll = (req, res, next) => {
-  res.status(200).send("Get All");
+const { BookModel } = require("../models/book");
+
+exports.getAll = async (req, res, next) => {
+  return await BookModel.findAll().then((books) => {
+    res.status(200).send(books);
+  });
 };
 
-exports.get = (req, res, next) => {
+exports.get = async (req, res, next) => {
   let id = req.params.id;
+  return await BookModel.findOne({
+    where: {
+      id: id,
+    },
+  }).then((book) => {
+    res.status(200).send(book);
+  });
 };
 
-exports.create = (req, res, next) => {};
-
-exports.update = (req, res, next) => {
-  let id = req.params.id;
+exports.create = async (req, res, next) => {
+  return await BookModel.create(req.body)
+    .then((book) => {
+      if (book) {
+        res.status(201).send(book);
+      } else {
+        res.status(400).send("Error ao adicionar o livro");
+      }
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
 };
 
-exports.delete = (req, res, next) => {
+exports.update = async (req, res, next) => {
   let id = req.params.id;
+  return await BookModel.update(req.body, {
+    where: {
+      id: id,
+    },
+  })
+    .then((book) => {
+      res.status(200).send(book);
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+};
+
+exports.delete = async (req, res, next) => {
+  let id = req.params.id;
+  return await BookModel.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(() => {
+      res.status(200).send(id);
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
 };
